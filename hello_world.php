9b7fb668cd8e8b138506a5128d0b8517909b75bd
xconstaud <xconstaud@gmail.com>
2017-03-13 14:35:18 -0500

    Add if.php

--- hello_world.php
+++ hello_world.php
@@ -3,6 +3,12 @@
 $firstName = "William";
 $lastName = "Staud";
 
-echo("Hello, my name is " . $firstName . " " . $lastName);
+echo("Hello, my name is " . $firstName . " " . $lastName . " " . PHP_EOL);
+
+if($firstName === "William"){
+    echo "Hi, William!" . PHP_EOL;
+}else{
+    echo "Who in the world are you?" . PHP_EOL;
+}
 
 ?>
