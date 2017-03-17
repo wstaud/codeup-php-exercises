af1976d8b2c2f35d17f6075d7fd7882332b7bb98
xconstaud <xconstaud@gmail.com>
2017-03-17 16:41:23 -0500

    Add user restrictions to input

--- arithmetic.php
+++ arithmetic.php
@@ -20,6 +20,30 @@ function divide($a, $b)
     echo($a / $b) . PHP_EOL;;
 }
 
+function checkError($operation, $a, $b) {
+    if ($operation == 0) {
+        echo ("Zero is not a choice") . PHP_EOL;
+        exit(0);
+    }else if (is_numeric($operation) == false || is_numeric($a) == false || is_numeric($b) == false) {
+        echo ("Numbers only!") .PHP_EOL;
+        exit(0);
+    }else {
+        if($operation == 1) {
+        add($a, $b);
+        }else if ($operation == 2) {
+            subtract($a, $b);
+        }else if ($operation == 3) {
+            multiply($a, $b);
+        }else if ($operation == 4) {
+            if ($b == 0){
+                echo ("Can't divide by zero") . PHP_EOL;
+                exit(0);
+            }
+            divide($a, $b);
+        }
+    }
+}
+
 echo("1: for add") . PHP_EOL;
 echo("2: for subtract") . PHP_EOL;
 echo("3: for multiply") . PHP_EOL;
@@ -29,18 +53,11 @@ echo("4: for divide") . PHP_EOL;
 fwrite(STDOUT, "First number? ");
 $a = trim(fgets(STDIN));
 
-fwrite(STDOUT, "Second number? ");
+fwrite(STDOUT, "Second number? "); 
 $b = trim(fgets(STDIN));
+checkError($operation, $a, $b);
 
 
-if($operation == 1) {
-    add($a, $b);
-}else if ($operation == 2) {
-    subtract($a, $b);
-}else if ($operation == 3) {
-    multiply($a, $b);
-}else if ($operation == 4) {
-    divide($a, $b);
-}
+
 
 
