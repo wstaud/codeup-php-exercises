630be7bafe78e58da8ce697e250a81043cdcdb75
xconstaud <xconstaud@gmail.com>
2017-03-17 16:00:24 -0500

    Update arithmetic.php with user input and calculations

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,45 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+    echo($a + $b) . PHP_EOL;;
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+    echo($a - $b) . PHP_EOL;;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    echo($a * $b) . PHP_EOL;;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    echo($a / $b) . PHP_EOL;;
 }
 
-// Add code to test your functions here
+echo("1: for add") . PHP_EOL;
+echo("2: for subtract") . PHP_EOL;
+echo("3: for multiply") . PHP_EOL;
+echo("4: for divide") . PHP_EOL;
+(int) $operation = trim(fgets(STDIN));
+
+fwrite(STDOUT, "First number? ");
+$a = trim(fgets(STDIN));
+
+fwrite(STDOUT, "Second number? ");
+$b = trim(fgets(STDIN));
+
+
+if($operation == 1) {
+    add($a, $b);
+}else if ($operation == 2) {
+    subtract($a, $b);
+}else if ($operation == 3) {
+    multiply($a, $b);
+}else if ($operation == 4) {
+    divide($a, $b);
+}
+
+
