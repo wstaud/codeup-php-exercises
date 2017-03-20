a079552ddf746be10bb6322c876f3cbafd62a5dd
xconstaud <xconstaud@gmail.com>
2017-03-20 11:35:16 -0500

    add tech-companies exercise

---
+++ technology-companies.php
@@ -0,0 +1,60 @@
+<?php
+
+
+
+$companies = [
+    'Sun Microsystems' => [
+        'Vinod Khosla',
+        'Bill Joy',
+        'Andy Bechtolsheim',
+        'Scott McNealy'
+    ],
+    'Silicon Graphics' => [
+        'Jim Clark',
+        'Ed McCracken'
+    ],
+    'Cray' => [
+        'William Norris',
+        'Seymour Cray'
+    ],
+    'NeXT' => [
+        'Steve Jobs',
+        'Avie Tevanian',
+        'Joanna Hoffman',
+        'Bud Tribble',
+        'Susan Kare'
+    ],
+    'Acorn Computers' => [
+        'Steve Furber',
+        'Sophie Wilson',
+        'Hermann Hauser',
+        'Jim Mitchell'
+    ],
+    'MIPS Technologies' => [
+        'Skip Stritter',
+        'John L. Hennessy'
+    ],
+    'Commodore' => [
+        'Yash Terakura',
+        'Bob Russell',
+        'Bob Yannes',
+        'David A. Ziembicki',
+        'Jay Miner'
+    ],
+    'Be Inc' => [
+        'Steve Sakoman',
+        'Jean-Louis Gassée'
+    ]
+];
+
+// Sorts the company keys
+ksort($companies);
+
+// goes through each company, asigns it's values as persons, goes through those and sorts them. Reassigns $companies
+foreach ($companies as $company => $persons) {
+    sort($persons);
+    $companies[$company] = $persons;
+}
+
+// Dumps the new companies array
+var_dump($companies);
