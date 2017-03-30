4dca8965f8b75bc8a6d93223b082ed051a7c0877
xconstaud <xconstaud@gmail.com>
2017-03-30 13:09:21 -0500

    Update Log exercise with constructor

--- Log.php
+++ Log.php
@@ -1,16 +1,13 @@
 <?PHP
 
-
 class Log {
     public $filename;
+    public $handle;
+    public $prefix = "log";
 
     public function logMessage($logLevel, $message) {
-        $this->filename = "txt/log-" . date("Y-m-d") . ".log"; 
-        $handle = fopen($this->filename, 'a');
-    
         $logLevel = date("Y-m-d") . " " . date("H:i:s") . " " . $logLevel;
-        fwrite($handle, $logLevel . ": " . $message . PHP_EOL);
-        fclose($handle);
+        fwrite($this->handle, $logLevel . ": " . $message . PHP_EOL);
     }  
 
     public function info($message) {
@@ -18,13 +15,18 @@ class Log {
     }
     public function error($message) {
         $this->logMessage("ERROR", $message); 
-
     }
 
-      
+    public function __construct($prefix) {
+        $this->prefix = $prefix; 
+        $this->filename = "$prefix-" . date("Y-m-d") . ".log";
+        $this->handle = fopen($this->filename, 'a');
+    }
+
+    public function __destruct(){
+        fclose($this->handle);
+    }
 
 }
-
-
 
 ?>
