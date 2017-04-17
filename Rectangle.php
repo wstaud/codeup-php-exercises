a88b03e99373cd055243ad243152779262f03b9c
xconstaud <xconstaud@gmail.com>
2017-04-17 09:38:31 -0500

    Update Square exercise with private and protected

--- Rectangle.php
+++ Rectangle.php
@@ -2,23 +2,41 @@
 
 class Rectangle {
 
-    public $height;
-    public $width;
+    private $height;
+    private $width;
 
     public function __construct($height, $width) {
+        $this->setHeight($height);
+        $this->setWidth($width);
+    }
+
+    protected function setHeight($height) {
         $this->height = $height;
+    }
+
+    protected function setWidth($width) {
         $this->width = $width;
     }
 
+    public function getHeight() {
+        return $this->height;
+    }
+
+    public function getWidth() {
+        return $this->width;
+    }
+
     public function area() { 
-        $area = $this->height * $this->width;
+        $area = $this->getHeight() * $this->getWidth();
 
         return $area;
     }
 
     public function perimeter() {
-        return ($this->height * 2) + ($this->width * 2);
+        return ($this->getHeight() * 2) + ($this->getWidth() * 2);
     }
+
+    
 }
 
 ?>
--- Square.php
+++ Square.php
@@ -4,17 +4,17 @@ require_once __DIR__ . '/Rectangle.php';
 class Square extends Rectangle {
 
     public function __construct($height) {
-        $this->height = $height;
+        parent::__construct($height, $height);
     }
 
     public function perimeter() {
-        $perimeter = $this->height * 4;
+        $perimeter = $this->getHeight() * 4;
 
         return $perimeter;
     }
 
     public function area() {
-        return $this->height * $this->height;
+        return $this->getHeight()* $this->getHeight();
     }
 
 }
